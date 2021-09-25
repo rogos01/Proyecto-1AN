23d1ee8fe8115bf4a511745b17a526d23ef0f929
rodrigo <54129928+rogos01@users.noreply.github.com>
2021-09-24 22:50:09 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#include "letrero.h"
+#include "instruccion.h"
+#include "funcion.h"
+#include "despedida.h"
+
+
+void instruccion();
+void Letrero();
+void funcion();
+void despedida();
+
+int main(){
+    int opc = 0;
+
+    do{
+        system("cls");
+        Letrero();
+        printf("\n\t\t\t\tOpciones\n");
+        printf("\nPor favor lea primero las instrucciones para asber como se usa el programa\n");
+        printf("\n\t\t\t\t[1]Instrucciones\n");
+        printf("\n\t\t\t\t[2]Primer tipo de funcion\n");
+        printf("\n\t\t\t\t[3]Segundo tipo de funcion\n");
+        printf("\n\t\t\t\t[4]Salir\n");
+        scanf("%d",&opc);
+        switch (opc){
+            case 1:
+                instruccion();
+                break;
+            case 2:
+                funcion();
+                break;
+            /*case 3:
+                funcion1();
+                break;*/
+            case 4:
+                despedida();
+                break;
+            
+            default:
+                printf("\n\tEleccion no recconocida");
+                break;
+            }
+        system("pause>nul");
+    }
+    while(opc != 2);
+    return 0;
+}
